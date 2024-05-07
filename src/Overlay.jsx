8f0988bcbe73645ab1446738fadb6a65262d5680
Christian <mail@christianhohenbild.de>
2024-05-07 23:46:12 +0200

    add tailwind

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -3,34 +3,48 @@ import { Scroll } from "@react-three/drei"
 function Overlay(){
 
   return  <Scroll html>
+  <div className="m-10 text-gray-500">
+  <div 
+  
+  style={{ height: "90vh" }}>
   <div>
-  <div style={{ height: "100vh" }}>
-  <div>
-    <h1>headset</h1>
-    Virtual reality (VR) is a simulated experience that can be similar to or completely different from the real world.
+    <h1
+    className="headings"
+    >headset</h1>
+    <p
+    className="text-2xl"
+    >Virtual reality (VR) is a simulated experience that can be similar to or completely different from the real world.</p>
   </div>
 </div>
-<div style={{ height: "100vh" }}>
+<div style={{ height: "90vh" }}>
   <div>
-    <h1>headphone</h1>
+    <h1
+    className="headings"
+    >headphone</h1>
     Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears.
   </div>
 </div>
-<div style={{ height: "100vh" }}>
+<div style={{ height: "90vh" }}>
   <div>
-    <h1>rocket</h1>A rocket (from Italian: rocchetto, lit. 'bobbin/spool')[nb 1][1] is a projectile that spacecraft, aircraft or other vehicle use to obtain thrust from a
+    <h1
+        className="headings"
+    >rocket</h1>A rocket (from Italian: rocchetto, lit. 'bobbin/spool')[nb 1][1] is a projectile that spacecraft, aircraft or other vehicle use to obtain thrust from a
     rocket engine.
   </div>
 </div>
-<div style={{ height: "100vh" }}>
+<div style={{ height: "90vh" }}>
   <div>
-    <h1>turbine</h1>A turbine (/ˈtɜːrbaɪn/ or /ˈtɜːrbɪn/) (from the Greek τύρβη, tyrbē, or Latin turbo, meaning vortex)[1][2] is a rotary mechanical device that extracts energy
+    <h1
+        className="headings"
+    >turbine</h1>A turbine (/ˈtɜːrbaɪn/ or /ˈtɜːrbɪn/) (from the Greek τύρβη, tyrbē, or Latin turbo, meaning vortex)[1][2] is a rotary mechanical device that extracts energy
     from a fluid flow and converts it into useful work.
   </div>
 </div>
 <div style={{ height: "100vh" }}>
   <div>
-    <h1>table</h1>A table is an item of furniture with a flat top and one or more legs, used as a surface for working at, eating from or on which to place things.[1][2]
+    <h1
+    className="headings"
+    >table</h1>A table is an item of furniture with a flat top and one or more legs, used as a surface for working at, eating from or on which to place things.[1][2]
   </div>
 </div>
 <div style={{ height: "100vh" }}>
@@ -40,7 +54,9 @@ function Overlay(){
 </div>
 <div style={{ height: "100vh" }}>
   <div>
-    <h1>zeppelin</h1>A Zeppelin is a type of rigid airship named after the German inventor Count Ferdinand von Zeppelin (German pronunciation: [ˈt͡sɛpəliːn]) who pioneered rigid
+    <h1
+    className="headings"
+    >zeppelin</h1>A Zeppelin is a type of rigid airship named after the German inventor Count Ferdinand von Zeppelin (German pronunciation: [ˈt͡sɛpəliːn]) who pioneered rigid
     airship development at the beginning of the 20th century.
   </div>
 </div>
