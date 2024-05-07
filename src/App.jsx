dafac9f46439513d578f9ed2d6b1b2d896aab948
Christian <mail@christianhohenbild.de>
2024-05-07 20:41:32 +0200

    add model and motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { Environment } from "@react-three/drei"
 
 import './index.css'
 
-import Experience from "./Experience"
+import Experience from "./Experience.jsx"
+import Model from "./Model.jsx"
 
 
 export default function App() {
 
  return (
 
-  
     <Canvas shadows camera={{ position: [0, 0, 4], fov: 40 }}>
       <Environment
         files="./textures/envmap.hdr" />
         <color 
           attach="background" 
-          args={["#eeeeee"]} />
-      <Experience />
+          args={["#222222"]} />
+      <Model />
     </Canvas>
   
   );
