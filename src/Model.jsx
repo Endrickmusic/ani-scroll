40038579c88552dcda43a6d5867c43b32a03c7bb
Christian <mail@christianhohenbild.de>
2024-06-26 19:39:44 +0200

    add text animation

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 import { MathUtils } from "three"
 import { useFrame } from "@react-three/fiber"
 import {
@@ -14,6 +14,14 @@ export default function Model({ ...props }) {
   const { nodes, materials, animations } = useGLTF("./models/ortho_02.glb")
   const { actions } = useAnimations(animations, group)
 
+  const [nameIsVisible, setNameIsVisible] = useState(true)
+  const [WorksIsVisible, setWorksIsVisible] = useState(false)
+  const [ProjectsIsVisible, setProjectsIsVisible] = useState(false)
+  const [Works2IsVisible, setWorks2IsVisible] = useState(false)
+  const [Works3IsVisible, setWorks3IsVisible] = useState(false)
+  const [Projects2IsVisible, setProjects2IsVisible] = useState(false)
+  const [ContactIsVisible, setContactIsVisible] = useState(false)
+
   const scrolling = useScroll()
 
   const extras = {
@@ -28,8 +36,14 @@ export default function Model({ ...props }) {
 
   useFrame((state) => {
     const scroll = scrolling.offset
-
-    console.log(scroll)
+    setNameIsVisible(scrolling.offset > -0.1 && scrolling.offset < 0.1)
+    setWorksIsVisible(scrolling.offset > 0.11 && scrolling.offset < 0.24)
+    setProjectsIsVisible(scrolling.offset > 0.18 && scrolling.offset < 0.33)
+    setWorks2IsVisible(scrolling.offset > 0.23 && scrolling.offset < 0.44)
+    setWorks3IsVisible(scrolling.offset > 0.34 && scrolling.offset < 0.64)
+    setProjects2IsVisible(scrolling.offset > 0.54 && scrolling.offset < 0.78)
+    setContactIsVisible(scrolling.offset > 0.68 && scrolling.offset < 0.85)
+    console.log(scrolling.offset)
 
     // textRef.current.material.opacity = 1 - scroll
     textRef.current.material.transparent = true
@@ -53,6 +67,7 @@ export default function Model({ ...props }) {
           position={[1.2, 1.2, -1]}
           rotation={[Math.PI / 2, 0, 0]}
           ref={textRef}
+          visible={nameIsVisible}
         />
         <mesh
           name="Portfolio"
@@ -62,6 +77,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[-1.8, -2.8, 6]}
           rotation={[Math.PI / 2, 0, 0]}
+          visible={nameIsVisible}
         />
         <mesh
           name="Object001"
@@ -79,6 +95,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[5.692, -3.8, 1.038]}
           rotation={[Math.PI / 2, 0, -Math.PI / 4]}
+          visible={WorksIsVisible}
         />
         <mesh
           name="Projects"
@@ -88,6 +105,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[7.692, -5.8, -1.182]}
           rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+          visible={ProjectsIsVisible}
         />
         <mesh
           name="Works2"
@@ -97,6 +115,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[-0.308, -5.8, 2.233]}
           rotation={[-Math.PI, -Math.PI / 2, 0]}
+          visible={Works2IsVisible}
         />
         <mesh
           name="Works3"
@@ -106,6 +125,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[1.2, -8.8, 6]}
           rotation={[Math.PI / 2, -Math.PI / 2, 0]}
+          visible={Works3IsVisible}
         />
         <mesh
           name="Projects2"
@@ -115,6 +135,7 @@ export default function Model({ ...props }) {
           material={materials.Schrift}
           position={[7.692, -8.345, -0.182]}
           rotation={[0, 0, -Math.PI / 2]}
+          visible={Projects2IsVisible}
         />
         <mesh
           name="Contact"
@@ -123,6 +144,7 @@ export default function Model({ ...props }) {
           geometry={nodes.Contact.geometry}
           material={materials.Schrift}
           position={[3.184, -2.8, 6]}
+          visible={ContactIsVisible}
         />
         <OrthographicCamera
           name="Camera"
